223b61e64775f52d09683fce60e297abcb71683c
jon-H34400 <ge38yub@mytum.de>
2026-07-17 13:28:47 +0200

    Fix start from introScene with start buton

--- Assets/janaStuff/introScript.cs
+++ Assets/janaStuff/introScript.cs
@@ -1,23 +1,31 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Controls;
 using UnityEngine.SceneManagement;
 
 public class introScript : MonoBehaviour
 {
-    private DancematTranslater danceMat;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        danceMat = FindObjectOfType<DancematTranslater>();
-    }
-
-    // Update is called once per frame
     void Update()
     {
-        if(Keyboard.current.nKey.wasPressedThisFrame
-            || (danceMat != null && danceMat.RestartPressedThisFrame()))
+        if (Keyboard.current.nKey.wasPressedThisFrame || StartButtonPressed())
         {
             SceneManager.LoadScene("Berrak");
         }
+    }
+
+    private bool StartButtonPressed()
+    {
+        foreach (var device in InputSystem.devices)
+        {
+            foreach (var control in device.allControls)
+            {
+                if (control is not ButtonControl button) continue;
+                // Linux: Gamepad-Layout
+                if (button.path.EndsWith("/start") && button.wasPressedThisFrame) return true;
+                // Windows: Joystick-Layout
+                if (button.name == "button10" && button.wasPressedThisFrame) return true;
+            }
+        }
+        return false;
     }
 }
